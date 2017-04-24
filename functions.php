65dff1863ee28586fdbc7778ad23564761aa7ff6
Adam Pearson <adampearson@adams-mbp.home>
2017-04-24 23:36:28 +0100

    implementing navigation bar, including styling for mobile

--- functions.php
+++ functions.php
@@ -6,6 +6,12 @@ function theme_styles_and_scripts() {
 	wp_enqueue_style( 'theme-css', get_template_directory_uri() . '/assets/css/style.css' );
 	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/assets/css/bootstrap.min.css' );
 	wp_enqueue_style( 'font-awesome-css', get_template_directory_uri() . '/assets/css/font-awesome.min.css' );
-	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), null, true );
+	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array( 'jquery' ), null, true );
+	wp_enqueue_script( 'custom-js', get_template_directory_uri() . '/assets/js/custom.js', array( 'jquery' ), null, true );
 }
 add_action( 'wp_enqueue_scripts', 'theme_styles_and_scripts' );
+
+/** 
+* Add theme support for custom logo
+**/
+add_theme_support( 'custom-logo' );
--- header.php
+++ header.php
@@ -1,10 +1,27 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<?php wp_head(); ?> 
+<html <?php language_attributes(); ?> xmlns="http://www.w3.org/1999/xhtml">
+<head> 
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<title><?php wp_title(); ?></title>
+	<?php wp_head(); ?>
 </head>
 <body class="<?php body_class(); ?>">
-
-</div>
+<header class="container-fluid">
+	<div class="col-xs-3" id="logo">
+		<?php if ( function_exists( 'the_custom_logo' ) )
+		{
+			the_custom_logo();
+		} ?>
+	</div>
+	<div class="col-xs-9" id="navigation">
+	<a class="toggle-mobile-menu">
+		<i class="mobile-menu-icon fa fa-bars"></i>
+	</a>
+		<?php $args = array(
+			'menu' => 'primary',
+			'container'	=>	'',
+		);
+		wp_nav_menu( $args ) ?>
+	</div>
+</header>
